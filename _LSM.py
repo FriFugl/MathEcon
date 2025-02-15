262c1c6a35abf5a6820112300f1c6a6a0b3b29cd
FriFugl <lassehpagh@gmail.com>
2025-02-15 18:46:20 +0100

    Changes

--- _LSM.py
+++ _LSM.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+from fontTools.merge.util import current_time
+
 from _calculate_swap_and_swaption_details import _calculate_swaption_payoffs
 
 
@@ -8,50 +10,51 @@ def _LSM_method(
     swap_rates: pd.DataFrame,
     accrual_factors: pd.DataFrame,
     strike: float,
-    N: int,
+    exercise_dates: list[float],
 ) -> pd.DataFrame:
     """
     Perform classic Longstaff-Schwartz algorithm for a swaption.
 
     short_rates: Simulated short rates.
-    swap_rates: Simulated swap rates corresponding to the short_rates
+    swap_rates: Simulated swap rates corresponding to the short_rates.
+    accrual_factors: Simulated swap rates corresponding to the short_rates.
     strike: Strike of Swaption.
+    exercise_dates: Swaption exercise dates.
     N: Number of simulated tracjetories.
     """
 
     payoffs = _calculate_swaption_payoffs(
         swap_rates=swap_rates, accrual_factors=accrual_factors, strike=strike
     )
+    discounts = np.exp(-short_rates.loc[:, exercise_dates[:-1]].iloc[0].values * np.diff(exercise_dates))
 
-    for i, col in enumerate(reversed(payoffs.columns)):
-        print(col)
-        if i == 0:
-            cashflows = payoffs[col]
-            continue
+    cashflows = payoffs[exercise_dates[-1]] * discounts[-1]
+    for i in range(len(exercise_dates) - 2, -1, -1):
+        t = exercise_dates[i]
 
-        discount = np.exp(
-            -short_rates[col].iloc[0]*(short_rates[col+1]-short_rates[col])
-        )
-
-        itm_paths = payoffs.index[payoffs[col] > 0].tolist()
+        itm_paths = payoffs.index[payoffs[t] > 0].tolist()
         if itm_paths == []:
-            cashflows = cashflows * discount
+            cashflows = cashflows * discounts[i-1]
             continue
 
-        itm_swap_rates = swap_rates.loc[itm_paths, col]
+        itm_swap_rates = swap_rates.loc[itm_paths, t]
         itm_cashflows = cashflows[itm_paths].to_numpy()
 
         X = np.column_stack(
             (np.ones(len(itm_swap_rates)), itm_swap_rates, itm_swap_rates**2)
         )
         beta, residuals, rank, s = np.linalg.lstsq(X, itm_cashflows, rcond=None)
-        print(beta)
+
         continuation_values = (
             beta[0] + beta[1] * itm_swap_rates + beta[2] * itm_swap_rates**2
         )
         exercised_paths = (
-            payoffs[col] > continuation_values.reindex(payoffs[col].index) * discount
+            payoffs[t] > continuation_values.reindex(payoffs[t].index)
         )
-        cashflows.loc[exercised_paths] = payoffs[col].loc[exercised_paths]
 
-    return sum(cashflows) / N
+        cashflows.loc[exercised_paths] = payoffs[t].loc[exercised_paths]
+
+        if t != 0:
+            cashflows = cashflows * discounts[i-1]
+
+    return sum(cashflows) / len(cashflows)
--- _calculate_swap_and_swaption_details.py
+++ _calculate_swap_and_swaption_details.py
@@ -11,15 +11,7 @@ def _compute_swap_rate_and_accrual_factor(
     zcb_prices: ZCB prices in a pandas dataframe starting from p(0,0) to some maturity.
     start: First resettlement.
     maturity: Maturity of swap.
-    """
-    """
-    start_index = zcb_prices.columns.get_loc(start)
-    maturity_index = zcb_prices.columns.get_loc(maturity)
-
-    accrual_factors = zcb_prices.iloc[:, 1 : maturity_index + 1].sum(axis=1)
-    swap_rate = (zcb_prices[start] - zcb_prices[maturity]) / accrual_factors
-
-    return swap_rate, accrual_factors
+    alpha: Time interval between swap fixings in years.
     """
 
     accrual_factors = alpha*zcb_prices.loc[:, start:maturity].sum(axis=1)
@@ -27,9 +19,60 @@ def _compute_swap_rate_and_accrual_factor(
 
     return swap_rates, accrual_factors
 
+    def _swap_rates_from_zcb_pr(
+        self,
+        short_rate: pd.DataFrame,
+        entry_dates: float,
+        expiry: float,
+        alpha: float
+    ) -> pd.DataFrame:
+        """
+        Convert short rates in a Vasicek model to swap rates.
+
+        short_rates: Simulated short rates.
+        entry_dates: List of dates for which swap rates is entered. Each date is equivalent to T_{n-1}.
+        expiry: Years until swap expiry. Equivalent to T_N.
+        """
+        swap_rates = {}
+        accrual_factors = {}
+
+        for date in entry_dates:
+            start_date = min(i for i in np.arange(0, expiry, alpha) if i > date + 1e-5)
+            swap_annuities = np.arange(start_date, expiry + alpha, alpha)
+
+            zcb_prices = self.price_zcb(
+                short_rates=short_rate, t=date, maturities=swap_annuities
+            )
+
+            R, S = _compute_swap_rate_and_accrual_factor(
+                zcb_prices=zcb_prices, start=start_date, maturity=expiry, alpha=alpha
+            )
+
+            swap_rates[date] = R
+            accrual_factors[date] = S
+
+        swap_rates = pd.DataFrame(swap_rates)
+        accrual_factors = pd.DataFrame(accrual_factors)
+
+        return swap_rates, accrual_factors
+
 
 
 def _calculate_swaption_payoffs(
-    swap_rates: pd.DataFrame, accrual_factors: pd.DataFrame, strike
+    swap_rates: pd.DataFrame, accrual_factors: pd.DataFrame, strike, payer: bool = True
 ) -> pd.DataFrame:
-    return accrual_factors * np.maximum(swap_rates - strike, 0)
+    """
+    Function to calculate swaption payoff.
+
+
+    swap_rates: Dataframe with swap rates at time points.
+    accrual_factors: Dataframe with accrual factors at time points.
+    strike: Strike rate of swaption.
+    payer: True for payer swaption, False for receiver swaption. Default is True.
+    """
+
+    if payer:
+        return accrual_factors * np.maximum(swap_rates - strike, 0)
+    else:
+        return accrual_factors * np.minimum(strike - swap_rates, 0)
+
